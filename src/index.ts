82fb1ff2d2bf4d8e84aa35c05feafff56cbf823f
cmarin001 <cmarin6051@gmail.com>
2024-07-25 17:54:02 -0500

    feat(stackItem): create and export StackItem

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { Stack } from "./Stack";
+export { StackItem } from "./StackItem";
 export type { IStack } from "./Stack";
+export type { IStackItem } from "./StackItem";
 export type {
   IStackAlignContent,
   IStackAlignItem,
@@ -7,3 +9,4 @@ export type {
   IStackJustifyContent,
   IStackWrapControl,
 } from "./Stack/props";
+export type { IStackAlignSelfProperties } from "./StackItem/props";
